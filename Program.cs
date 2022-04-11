046c6e1d7b40bbb60943148562db98ddb49864a8
parthgarg977 <67135234+Parthgarg977@users.noreply.github.com>
2022-04-11 16:12:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LambdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the lambda problems");
+            LambdaFunction lambda = new LambdaFunction(); // calling
+            lambda.Validate();
+        }
+    }
+}
